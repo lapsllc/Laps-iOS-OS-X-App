f8aeab70c0bbf85e4a8a22c7ea012d0719b31617
Efe Helvaci <efe.459@gmail.com>
2016-02-15 20:24:31 +0200

    initial commit

---
+++ Lapsapp/Lapsapp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Lapsapp
+//
+//  Created by Efe Helvaci on 15/02/16.
+//  Copyright © 2016 Laps LLC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
